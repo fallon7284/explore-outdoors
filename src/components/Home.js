8864f9e1061cf5607aba801144ad91c3e74ecc60
Brendan Fallon <brendanc.fallon@gmail.com>
2019-09-10 16:03:24 -0400

    built list view, realize my hikes/boulders/camps are fetched from map component, need to move that logic up to top level and pass data/fetch functions down so I can toggle views

--- src/components/Home.js
+++ src/components/Home.js
@@ -111,7 +111,10 @@ export default class Home extends React.Component{
                     myLocation={this.state.myLocation}
                     filter={this.state.filter}
                     /> 
-                    : <div >This is the list view</div>}
+                    : <List 
+                        boulders={this.state.boulders} 
+                        camps={this.state.camps}
+                        hikes={this.state.hikes}/>}
                 </div>
             </div>       
         )
