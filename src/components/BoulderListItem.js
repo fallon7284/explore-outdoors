f6f7acc6c6fb3d8db85c99265ef6212f89a28745
Brendan Fallon <brendanc.fallon@gmail.com>
2019-09-10 15:47:50 -0400

    built reusable list item component, and hike/camp/boulder components that it forms

---
+++ src/components/BoulderListItem.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ListItem from './ListItem'
+
+export default ({name, area}) => {
+    return (
+        <div className="boulder-list-item">
+            <ListItem />
+        </div>
+    )
+}
---
+++ src/components/HikeListItem.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ListItem from './ListItem'
+
+export default ({name, area}) => {
+    return (
+        <div className="hike-list-item">
+            <ListItem />
+        </div>
+    )
+}
