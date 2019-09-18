aebf1f6ee63f03c399bb8eab5e49b784034fc83c
Brendan Fallon <brendanc.fallon@gmail.com>
2019-09-18 13:48:24 -0400

    replaces call to MP api with call to own server which calls MP api and caches data

--- src/components/Map.js
+++ src/components/Map.js
@@ -14,9 +14,9 @@ class Map extends React.Component {
       campgrounds: [],
       hikes: [],
       boulders: [],
-      mapView: true,
     }
   }
+
 
 
   componentDidUpdate(oldProps){
@@ -34,15 +34,8 @@ class Map extends React.Component {
   
   async getCampgrounds(lat, lng){
     try{
-      const { data } = await axios.get(`https://www.hikingproject.com/data/get-campgrounds?lat=${lat}&lon=${lng}&maxResults=50&maxDistance=50&key=${hikingProjectKey}`)
-      console.log(data)
-      const campgrounds = data.campgrounds.filter(c => {return c.isCampground && c.numCampsites > 0})
-      const formattedCampgrounds = campgrounds.map(c => {
-        const { imgUrl, name, location, latitude, longitude, isBookable, isCampground, url, numCampsites } = c
-        return { imgUrl, name, location, latitude, longitude, isBookable, isCampground, url, numCampsites}
-      })
-      const camps = await axios.post('http://explore-outdoors-backend.herokuapp.com/camps', formattedCampgrounds)
-      this.setState({campgrounds: [...this.state.campgrounds, ...camps.data]})
+      const { data } = await axios.get(`http://explore-outdoors-backend.herokuapp.com/camps?lat=${lat}&lon=${lng}&maxResults=50&maxDistance=50&key=${hikingProjectKey}`)
+      this.setState({campgrounds: data})
     }catch(error){
       console.log(error)
     }
@@ -50,31 +43,17 @@ class Map extends React.Component {
 
   async getTrails(lat, lng){
     try{
-      const { data } = await axios.get(`https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lng}&maxResults=75&minStars=4&minLength=5&maxDistance=50&key=${hikingProjectKey}`)
-      const formattedHikes = data.trails.map(t => {
-        const { ascent, conditionDetails, conditionStatus, descent, difficulty, high, imgMedium, latitude,
-          length, location, longitude, low, name, summary, url} = t
-          return { ascent, conditionDetails, conditionStatus, descent, difficulty, high, imgMedium, latitude,
-            length, location, longitude, low, name, summary, url}
-      }) 
-      const hikes = await axios.post('http://explore-outdoors-backend.herokuapp.com/hikes', formattedHikes)
-      this.setState({hikes: [...this.state.hikes, ...hikes.data]})
+      const { data } = await axios.get(`http://explore-outdoors-backend.herokuapp.com/hikes?lat=${lat}&lon=${lng}&maxResults=75&minStars=4&minLength=5&maxDistance=50&key=${hikingProjectKey}`)
+      this.setState({hikes: data})
     } catch(error){
       console.log(error)
     }
   }
 
-  async getBoulders(lat, lng, maxV = 4, minV = 0){
+  async getBoulders(lat, lon, maxV = 4, minV = 0){
     try{
-      const { data } = await axios.get(`https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=${lat}&lon=${lng}&maxDistance=50&minDiff=V${minV}&maxDiff=V${maxV}&key=${mountainProjectKey}`)
-      const formattedBoulders = data.routes.map(t => {
-        const { name, rating, stars, location,
-          latitude, longitude, imgSmall, imgMedium, url} = t
-          return { name, rating, stars, location,
-            latitude, longitude, imgSmall, imgMedium, url}
-      }) 
-      const boulders = await axios.post('http://explore-outdoors-backend.herokuapp.com/boulders', formattedBoulders)
-      this.setState({boulders: [...this.state.boulders, ...boulders.data]})
+      const { data } = await axios.get(`http://explore-outdoors-backend.herokuapp.com/boulders?lat=${lat}&lon=${lon}&maxDistance=50&minDiff=V${minV}&maxDiff=V${maxV}&key=${mountainProjectKey}`)
+      this.setState({boulders: data})
     } catch(error){
       console.log(error)
     }
@@ -82,80 +61,99 @@ class Map extends React.Component {
 
   render() {
     const { lat, lng, name } = this.props.center.lat && this.props.center.lng ? this.props.center : (this.props.pins[0] ? this.props.pins[0] : {lat: null, lng: null})
-    console.log(this.props.mapView)
+    console.log('lat:', lat, 'lon:', lng)
     return (
-      <div>
-        {this.props.mapView ? <div style={{ height: '50vh', width: '50%', alignSelf: 'center' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: mapsKey}}
-          center={{lat, lng}}
-          defaultCenter={{lat: 0, lng: 0}}
-          defaultZoom={this.props.zoom}
-        >
-          <DisplayContainer
-            lat={lat}
-            lng={lng}
-            text={name ? name : "Current Location"}
-            type='current'
-          />
-          {this.props.filter.pins && this.props.pins.length && this.props.pins.map((p, i)=> {
-            return (
-            <DisplayContainer
-            key={i}
-            lat={p.lat}
-            lng={p.lng}
-            text={p.name}
-            name={p.name ? p.name : ''}
-            type='pin'
-          />
-          )
-        })}
-          {this.props.filter.hikes && this.state.hikes.length && this.state.hikes.map((h, i) => {
-            return (
+      <div className="map-section" >
+        <div >
+            <div style={{height: '50vh', width: '50%', alignSelf: 'center'}}>
+            <GoogleMapReact
+              bootstrapURLKeys={{ key: mapsKey}}
+              center={{lat, lng}}
+              defaultCenter={{lat: 0, lng: 0}}
+              defaultZoom={this.props.zoom}
+              style={{border: '3px brown'}}
+            >
               <DisplayContainer
-              key={i}
-              lat={h.latitude}
-              lng={h.longitude}
-              text={h.name}
-              type='hike'
-              area={h}
+                lat={lat}
+                lng={lng}
+                text={name ? name : "Current Location"}
+                type='current'
               />
-            )
-          })}
-          {this.props.filter.camps && this.state.campgrounds.length && this.state.campgrounds.map((c, i) => {
-            return (
-              <DisplayContainer 
-              key={i}
-              lat={c.latitude}
-              lng={c.longitude}
-              text={c.name}
-              type='camp'
-              area={c}
+              {this.props.filter.pins && this.props.pins.length && this.props.pins.map((p, i)=> {
+                return (
+                <DisplayContainer
+                key={i}
+                lat={p.lat}
+                lng={p.lng}
+                text={p.name}
+                name={p.name ? p.name : ''}
+                type='pin'
               />
-            )
-          })}
-          {this.props.filter.boulders && this.state.boulders.length && this.state.boulders.map((c, i) => {
-            return (
-              <DisplayContainer 
-              key={i}
-              lat={c.latitude}
-              lng={c.longitude}
-              text={c.name}
-              type='boulder'
-              area={c}
-              />
-            )
-          })}
-        </GoogleMapReact>
-        </div>
-        : <div style={{backgroundColor: 'white'}}>LIST VIEW
-        <List 
-          camps={this.state.campgrounds} 
-          hikes={this.state.hikes} 
-          boulders={this.state.boulders}/>
-        </div>
-        }
-      </div>
+              )
+            })}
+              {this.props.filter.hikes && this.state.hikes.length && this.state.hikes.map((h, i) => {
+                return (
+                  <DisplayContainer
+                  key={i}
+                  lat={h.latitude}
+                  lng={h.longitude}
+                  text={h.name}
+                  type='hike'
+                  area={h}
+                  />
+                )
+              })}
+              {this.props.filter.camps && this.state.campgrounds.length && this.state.campgrounds.map((c, i) => {
+                return (
+                  <DisplayContainer 
+                  key={i}
+                  lat={c.latitude}
+                  lng={c.longitude}
+                  text={c.name}
+                  type='camp'
+                  area={c}
+                  />
+                )
+              })}
+              {this.props.filter.boulders && this.state.boulders.length && this.state.boulders.map((c, i) => {
+                return (
+                  <DisplayContainer 
+                  key={i}
+                  lat={c.latitude}
+                  lng={c.longitude}
+                  text={c.name}
+                  type='boulder'
+                  area={c}
+                  />
+                )
+              })}
+            </GoogleMapReact>
+            </div>
+            {!this.props.mapView && 
+            <div>
+            <List 
+            camps={this.state.campgrounds} 
+            hikes={this.state.hikes} 
+            boulders={this.state.boulders}/>
+
+
+            <div style={{position: 'fixed', width: '100%', display: 'flex', height: '100vh', justifyContent: 'space-evenly', backgroundColor: 'white', zIndex: 9999, top: 0}}>
+              <div style={{flexGrow: '1'}}>
+                Camps
+              </div>
+              <div style={{flexGrow: '1'}}>
+                Hikes
+              </div>
+              <div style={{flexGrow: '1'}}>
+                Boulders
+              </div>
+            </div>
+
+            </div>
+
+            }
+            </div>
+         </div>
     );
   }
 }
