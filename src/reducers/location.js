ffa8f7c0ba1d669f5b5d8cff0a5ea2ec3c4f2e00
Brendan Fallon <brendanc.fallon@gmail.com>
2019-10-23 16:44:23 -0400

    moves custom location fetching logic through redux store

--- src/reducers/location.js
+++ src/reducers/location.js
@@ -9,14 +9,29 @@ export const setLocation = (location) => {
 }
 
 
+export const fetchCustomLocation = (add) => {
+    return async (dispatch) => {
+        if (add.length){
+            add = add.split(' ').join('+')
+            const data = await axios.post(`https://maps.googleapis.com/maps/api/geocode/json?address=${add}&key=${mapsKey}`)
+            if(data.data.results.length){
+                const location = data.data.results[0].geometry.location
+                dispatch(setLocation(location))
+            }
+            else {
+                alert('Location not found')
+            }
+        }
+    }
+}
+
 
 export const fetchLocation = () => {
-    console.log("TRYING TO FETCH LOCATION FROM REDUX")
     return async (dispatch) => {
         const { data } = await axios.post(`https://www.googleapis.com/geolocation/v1/geolocate?key=${mapsKey}`)
         dispatch(setLocation(data.location))
     }
-} 
+}
 
 
 const initialState = {lat: null, lng: null}
