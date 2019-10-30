d19cc4ad49ccfe26e6777f4adc9762c3b46501d9
Brendan Fallon <brendanc.fallon@gmail.com>
2019-10-30 00:50:16 -0400

    layout fixes

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -10,34 +10,31 @@ const List = (props) => {
     const sortFilter = props.sortFilter
     const sortedHikes = sort(props.hikes, sortFilter)
     const sortedCamps = sort(props.camps, sortFilter)
+    console.log(props.camps)
     const sortedBoulders = sort(props.boulders, sortFilter)
     return (
-        <div className="list-page" 
-            // style={{display: 'flex', flexDirection: 'column'}}
-        >
-            <div className="list-title-bar" style={{width: '100%'}}>
-                <div className="list-title">Campgrounds</div>
-                <div className="list-title">Hiking Trails</div>
-                <div className="list-title">Boulder Problems</div>
-            </div>
-            <div 
-                className="list-page-body"
-                // style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', position: 'fixed', top: '15vh', height: '85vh'}}
-            >
-                <div style={{overflowY: 'scroll', width: '30%'}}>
-                {sortedCamps.map((c, i)=> {
-                    return <CampListItem toggleFullPage={() => toggleFullPage(c, 'camp')} name={c.name} area={c.area} key={`camp${i}`} image={c.imgUrl}/>
-                })}
+        <div className="list-page" >
+            <div className="list-title-bar" style={{width: '88%', display: 'grid', gridTemplateColumns: '1fr 1fr 1fr'}}>
+                <div>Campgrounds
+                    <div style={{overflowY: 'scroll', position: 'fixed', height: '100%', width: '25%', justifySelf: 'center'}}>
+                    {sortedCamps.map((c, i)=> {
+                        return <CampListItem toggleFullPage={() => toggleFullPage(c, 'camp')} name={c.name} area={c.area} key={`camp${i}`} image={c.imgUrl}/>
+                    })}
+                    </div>
                 </div>
-                <div style={{overflowY: 'scroll', width: '30%'}}>
-                {sortedHikes.map((h, i)=> {
-                    return <HikeListItem toggleFullPage={() => toggleFullPage(h, 'hike')} name={h.name} area={h.area} key={`hike${i}`} image={h.imgMedium}/>
-                })}
+                <div>Hiking Trails
+                    <div style={{overflowY: 'scroll', position: 'fixed', height: '100%', width: '25%', justifySelf: 'center'}}>
+                    {sortedHikes.map((h, i)=> {
+                        return <HikeListItem toggleFullPage={() => toggleFullPage(h, 'hike')} name={h.name} area={h.area} key={`hike${i}`} image={h.imgMedium}/>
+                    })}
+                    </div>
                 </div>
-                <div style={{overflowY: 'scroll', width: '30%'}}>
-                {sortedBoulders.map((b, i)=> {
-                    return <BoulderListItem toggleFullPage={() => toggleFullPage(b, 'boulder')} name={b.name} area={b.location[0]} key={`boulder${i}`} image={b.imgSmall ? b.imgSmall : b.imgMedium}/>
-                })}
+                <div>Boulder Problems
+                    <div style={{overflowY: 'scroll', position: 'fixed', height: '100%', width: '25%', justifySelf: 'center'}}>
+                    {sortedBoulders.map((b, i)=> {
+                        return <BoulderListItem toggleFullPage={() => toggleFullPage(b, 'boulder')} name={b.name} area={b.location[0]} key={`boulder${i}`} image={b.imgSmall ? b.imgSmall : b.imgMedium}/>
+                    })}
+                    </div>
                 </div>
             </div>
         </div>
